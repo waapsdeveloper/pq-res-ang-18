78a9b26ede238d95fee55e2fd6dbcf74d65d83c3
abdullah hanif <99804964+waapsdeveloper@users.noreply.github.com>
2024-11-19 19:26:03 +0500

    login register routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,43 +4,54 @@ import { AdminComponent } from './theme/layout/admin/admin.component';
 import { GuestComponent } from './theme/layout/guest/guest.component';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: AdminComponent,
+  //   children: [
+  //     {
+  //       path: '',
+  //       redirectTo: '/default',
+  //       pathMatch: 'full'
+  //     },
+  //     {
+  //       path: 'default',
+  //       loadComponent: () => import('./demo/default/default.component').then((c) => c.DefaultComponent)
+  //     },
+  //     {
+  //       path: 'typography',
+  //       loadComponent: () => import('./demo/elements/typography/typography.component')
+  //     },
+  //     {
+  //       path: 'color',
+  //       loadComponent: () => import('./demo/elements/element-color/element-color.component')
+  //     },
+  //     {
+  //       path: 'sample-page',
+  //       loadComponent: () => import('./demo/sample-page/sample-page.component')
+  //     }
+  //   ]
+  // },
+  // {
+  //   path: '',
+  //   component: GuestComponent,
+  //   children: [
+  //     {
+  //       path: 'guest',
+  //       loadChildren: () => import('./demo/pages/authentication/authentication.module').then((m) => m.AuthenticationModule)
+  //     }
+  //   ]
+  // }
+
   {
     path: '',
-    component: AdminComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: '/default',
-        pathMatch: 'full'
-      },
-      {
-        path: 'default',
-        loadComponent: () => import('./demo/default/default.component').then((c) => c.DefaultComponent)
-      },
-      {
-        path: 'typography',
-        loadComponent: () => import('./demo/elements/typography/typography.component')
-      },
-      {
-        path: 'color',
-        loadComponent: () => import('./demo/elements/element-color/element-color.component')
-      },
-      {
-        path: 'sample-page',
-        loadComponent: () => import('./demo/sample-page/sample-page.component')
-      }
-    ]
+    redirectTo: '/pages',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    component: GuestComponent,
-    children: [
-      {
-        path: 'guest',
-        loadChildren: () => import('./demo/pages/authentication/authentication.module').then((m) => m.AuthenticationModule)
-      }
-    ]
-  }
+    path: 'pages',
+    loadChildren: () => import('./pages/pages.module').then((m) => m.PagesModule)
+  },
+
 ];
 
 @NgModule({
