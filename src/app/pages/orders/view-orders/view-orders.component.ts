3da6f251162395d4e08bb75c6df7997a2a7062a8
ShayanBhatti <shayanbhatti332@gmail.com>
2024-12-05 18:07:43 +0500

    Rtable

--- src/app/pages/orders/view-orders/view-orders.component.ts
+++ src/app/pages/orders/view-orders/view-orders.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavService } from 'src/app/services/basic/nav.service';
+import { NetworkService } from 'src/app/services/network.service';
+
 
 @Component({
   selector: 'app-view-orders',
   templateUrl: './view-orders.component.html',
   styleUrl: './view-orders.component.scss'
 })
-export class ViewOrdersComponent {
+export class ViewOrdersComponent { 
   itemId;
   item;
-}
+
+  constructor(private nav: NavService, private network: NetworkService, public activatedRoute: ActivatedRoute) {
+    this.initialize();
+  }
+
+  async initialize() {
+    console.log("yayay");
+
+    const rew = await this.activatedRoute.snapshot.params;
+
+
+    this.itemId = rew['id'];
+
+
+    const res = await this.network.getTablesById(this.itemId);
+    console.log(res);
+    
+
+    this.item = res.Rtable;
+    
+
+}}
