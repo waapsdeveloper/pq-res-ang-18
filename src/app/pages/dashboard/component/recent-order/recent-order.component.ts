d96b2382f0a8039a1d0bef9191ad9c3f0f8c245f
ShayanBhatti <shayanbhatti332@gmail.com>
2024-12-13 16:52:08 +0500

    kkk

---
+++ src/app/pages/dashboard/component/recent-order/recent-order.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recent-order',
+  templateUrl: './recent-order.component.html',
+  styleUrl: './recent-order.component.scss'
+})
+export class RecentOrderComponent {
+
+}
