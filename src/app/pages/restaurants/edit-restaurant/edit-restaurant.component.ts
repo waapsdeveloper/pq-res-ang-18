e89cab680a9e99ccfb4c1132020b8b42b5e2cc4c
Muhammad Shayan Bhatti <shayanbhatti332@gmail.com>
2024-12-25 14:57:17 +0500

    edit restuarant

--- src/app/pages/restaurants/edit-restaurant/edit-restaurant.component.ts
+++ src/app/pages/restaurants/edit-restaurant/edit-restaurant.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { FormlyFieldConfig } from '@ngx-formly/core';
 import { NavService } from 'src/app/services/basic/nav.service';
-
 @Component({
   selector: 'app-edit-restaurant',
   templateUrl: './edit-restaurant.component.html',
@@ -29,6 +30,195 @@ export class EditRestaurantComponent implements OnInit {
     // Fetch the data from the server
 
   }
+ form = new FormGroup({});
+   model = {
+    name: 'Restaurant one',
+    image: '',
+    address: '',
+    phone: '8957985674',
+    email: 'restaurant1@mail.com',
+    website: '',
+    opening_hours: '',
+    // schedule: [
+    //   { day: 'Monday', status: false, start_time: '', end_time: '' },
+    //   { day: 'Tuesday', status: false, start_time: '', end_time: '' },
+    //   { day: 'Wednesday', status: false, start_time: '', end_time: '' },
+    //   { day: 'Thursday', status: false, start_time: '', end_time: '' },
+    //   { day: 'Friday', status: false, start_time: '', end_time: '' },
+    //   { day: 'Saturday', status: false, start_time: '', end_time: '' },
+    //   { day: 'Sunday', status: false, start_time: '', end_time: '' },
+    // ],
+    description: '',
+    rating: Math.floor(Math.random() * 6),
+    status: 'active',
+  };
+
+  fields: FormlyFieldConfig[] = [
+    {
+      fieldGroupClassName: 'row', // Bootstrap row
+      fieldGroup: [
+        {
+          key: 'name',
+          type: 'input',
+          props: {
+            label: 'Restaurant Name',
+            placeholder: 'Enter restaurant name',
+            required: true,
+            minLength: 3
+          },
+          className: 'col-md-4 col-12' // 3 columns on md+, full width on small screens
+        },
+
+        {
+          key: 'address',
+          type: 'input',
+          props: {
+            label: 'Address',
+            placeholder: 'Enter address',
+            required: true
+          },
+          className: 'col-md-4 col-12'
+        },
+        {
+          key: 'phone',
+          type: 'input',
+          props: {
+            label: 'Phone Number',
+            placeholder: 'Enter phone number',
+            // pattern: /^[0-9]{10,15}$/
+          },
+          className: 'col-md-4 col-12'
+        },
+        {
+          key: 'email',
+          type: 'input',
+          props: {
+            label: 'Email Address',
+            placeholder: 'Enter email',
+            type: 'email'
+          },
+          className: 'col-md-4 col-12'
+        },
+        {
+          key: 'website',
+          type: 'input',
+          props: {
+            label: 'Website',
+            placeholder: 'Enter website URL',
+            pattern: /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w .-]*)*\/?$/
+          },
+          className: 'col-md-4 col-12'
+        },
+
+        {
+          key: 'description',
+          type: 'textarea',
+          props: {
+            label: 'Description',
+            placeholder: 'Enter description'
+          },
+          className: 'col-md-4 col-12'
+        },
+
+
+        // {
+        //   key: 'image',
+        //   type: 'input',
+        //   props: {
+        //     label: 'Image',
+        //     placeholder: 'Enter image URL',
+        //     type: 'file'
+        //   },
+        //   className: 'col-md-4 col-12'
+        // },
+        // {
+        //   key: 'rating',
+        //   type: 'input',
+        //   props: {
+        //     label: 'Rating',
+        //     placeholder: 'Enter rating (0-5)',
+        //     type: 'number',
+        //     min: 0,
+        //     max: 5,
+        //     pattern: /^\d+(\.\d{1,2})?$/
+        //   },
+        //   className: 'col-md-4 col-12'
+        // },
+        {
+          key: 'status',
+          type: 'select',
+          props: {
+            label: 'Status',
+            options: [
+              { value: 'active', label: 'Active' },
+              { value: 'inactive', label: 'Inactive' }
+            ]
+          },
+          className: 'col-md-4 col-12'
+        },
+      ],
+    },
+//     {
+//       key: 'schedule_table',
+//       fieldGroupClassName: 'col-12 table-responsive', // Full-width table
+//       fieldGroup: [
+//         {
+//           key: 'schedule',
+//           fieldGroupClassName: 'row',
+//           fieldGroup: [
+//             // Define each day of the week as a separate field group
+//             ...['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].map((day) => ({
+//               fieldGroupClassName: 'row border p-2', // Individual row for each day
+//               fieldGroup: [
+//                 {
+//                   key: `${day.toLowerCase()}_day`, // Unique key for each day
+//                   type: 'input',
+//                   props: {
+//                     label: 'Day',
+//                     value: day,
+//                     disabled: true, // Static day name
+//                   },
+//                   className: 'col-md-3 col-12',
+//                 },
+//                 {
+//                   key: `${day.toLowerCase()}_status`,
+//                   type: 'checkbox',
+//                   props: {
+//                     label: 'Active',
+//                   },
+//                   className: 'col-md-3 col-12',
+//                 },
+//                 {
+//                   key: `${day.toLowerCase()}_start_time`,
+//                   type: 'input',
+//                   props: {
+//                     label: 'Start Time',
+//                     type: 'time',
+//                   },
+//                   className: 'col-md-3 col-12',
+//                 },
+//                 {
+//                   key: `${day.toLowerCase()}_end_time`,
+//                   type: 'input',
+//                   props: {
+//                     label: 'End Time',
+//                     type: 'time',
+//                   },
+//                   className: 'col-md-3 col-12',
+//                 },
+//               ],
+//             })),
+//           ],
+
+//       props: {
+//         label: 'Weekly Schedule',
+//         description: 'Set the schedule for each day of the week.',
+//       },
+//     },
+//   ],
+// },
+   ];
+   onSubmit(model){}
 
 
 
--- src/app/pages/restaurants/edit-restaurant/edit-restaurant.module.ts
+++ src/app/pages/restaurants/edit-restaurant/edit-restaurant.module.ts
@@ -3,15 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { EditRestaurantRoutingModule } from './edit-restaurant-routing.module';
 import { EditRestaurantComponent } from './edit-restaurant.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { KtAppToolbarModule } from 'src/app/components/kt-app-toolbar/kt-app-toolbar.module';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+import { KtAppFormPageModule } from 'src/app/components/layouts/kt-app-form-page/kt-app-form-page.module';
 
 @NgModule({
-  declarations: [
-    EditRestaurantComponent
-  ],
+  declarations: [EditRestaurantComponent],
   imports: [
     CommonModule,
-    EditRestaurantRoutingModule
+    EditRestaurantRoutingModule,
+    FormsModule,
+    FormlyModule,
+    ReactiveFormsModule,
+    FormlyBootstrapModule,
+    KtAppFormPageModule
   ]
 })
-export class EditRestaurantModule { }
+export class EditRestaurantModule {}
