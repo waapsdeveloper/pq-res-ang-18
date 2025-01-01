c2ecd60003f005f60283bcf93f85fedb52287d57
Muhammad Shayan Bhatti <shayanbhatti332@gmail.com>
2025-01-01 15:47:40 +0500

    sss

--- src/app/pages/rtables/list-rtables/list-rtables.module.ts
+++ src/app/pages/rtables/list-rtables/list-rtables.module.ts
@@ -8,7 +8,6 @@ import { ListRtablesComponent } from './list-rtables.component';
 import { KtAppListPageTableModule } from 'src/app/components/layouts/kt-list-page/kt-app-list-page-table/kt-app-list-page-table.module';
 import { KtListPageModule } from 'src/app/components/layouts/kt-list-page/kt-list-page.module';
 
-
 @NgModule({
   declarations: [
     ListRtablesComponent
