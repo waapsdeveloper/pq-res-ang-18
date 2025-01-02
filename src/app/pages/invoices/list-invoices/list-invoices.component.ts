a973e8bd1a599a67a06f751fc77aa0eaafb2bf69
Muhammad Shayan Bhatti <shayanbhatti332@gmail.com>
2025-01-02 12:56:59 +0500

    ss

--- src/app/pages/invoices/list-invoices/list-invoices.component.ts
+++ src/app/pages/invoices/list-invoices/list-invoices.component.ts
@@ -14,7 +14,7 @@ import { FormlyFieldConfig } from '@ngx-formly/core';
   styleUrls: ['./list-invoices.component.scss'],
 })
 export class ListInvoicesComponent  extends ListBlade{
-  columns: any[] = ['Products', 'Quantity', 'Price', 'Total Price', 'Status'];
+  columns: any[] = ['Invoice Number', 'Date', 'Method', 'Total Price', 'Status'];
   showEdit = false;
   title = 'Invoices';
   addurl = '/pages/invoices/add';
