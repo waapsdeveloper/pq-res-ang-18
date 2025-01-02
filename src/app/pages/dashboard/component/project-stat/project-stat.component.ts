2a1d7b8b2add6a5aecab3c1a643b428961b150ef
Muhammad Shayan Bhatti <shayanbhatti332@gmail.com>
2025-01-02 11:41:51 +0500

    ...

--- src/app/pages/dashboard/component/project-stat/project-stat.component.ts
+++ src/app/pages/dashboard/component/project-stat/project-stat.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { NetworkService } from 'src/app/services/network.service';
 
@@ -5,6 +6,7 @@ import { NetworkService } from 'src/app/services/network.service';
   selector: 'app-project-stat',
   templateUrl: './project-stat.component.html',
   styleUrl: './project-stat.component.scss',
+  imports: [CommonModule],
   standalone: true
 })
 export class ProjectStatComponent implements OnInit {
@@ -13,17 +15,18 @@ export class ProjectStatComponent implements OnInit {
   async ngOnInit() {
     const data = await this.network.getMSellingProduct();
     const d = data.order_products;
-    console.log(data);
+    console.log(d);
 
-    this.products = d.map((item) => ({
-      productId: item.product_id,
+    this.products = d.map(item => ({
+     productId: item.product_id,
       name: item.product.name,
       description: item.product.description,
-      price: parseFloat(item.product.price), // Assuming price is a string and needs to be converted to a number
-      discount: parseFloat(item.product.discount), // Assuming discount is a string
+      price: parseFloat(item.product.price), // Convert price to number
+      discount: parseFloat(item.product.discount), // Convert discount to number
       image: item.product.image,
       totalQuantitySold: item.total_quantity_sell
     }));
-    console.log(this.products);
+  console.log(this.products);
+
   }
 }
