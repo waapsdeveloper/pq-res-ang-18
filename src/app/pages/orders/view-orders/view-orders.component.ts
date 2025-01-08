8c112d1ecd793d79e96822d2264fd99e637646e7
Muhammad Shayan Bhatti <shayanbhatti332@gmail.com>
2025-01-08 20:00:50 +0500

    ss

--- src/app/pages/orders/view-orders/view-orders.component.ts
+++ src/app/pages/orders/view-orders/view-orders.component.ts
@@ -2,18 +2,22 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NavService } from 'src/app/services/basic/nav.service';
 import { NetworkService } from 'src/app/services/network.service';
-
+import { NgbPopover } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-view-orders',
   templateUrl: './view-orders.component.html',
   styleUrl: './view-orders.component.scss'
 })
-export class ViewOrdersComponent { 
+export class ViewOrdersComponent {
   itemId;
   item;
 
-  constructor(private nav: NavService, private network: NetworkService, public activatedRoute: ActivatedRoute) {
+  constructor(
+    private nav: NavService,
+    private network: NetworkService,
+    public activatedRoute: ActivatedRoute
+  ) {
     this.initialize();
   }
 
@@ -22,9 +26,15 @@ export class ViewOrdersComponent {
     this.itemId = rew['id'];
     const res = await this.network.getOrdersById(this.itemId);
     console.log(res);
-    
 
     this.item = res.order;
-    
+  }
+  popovers: NgbPopover[] = [];
+  closeAllPopovers() {
+    this.popovers.forEach((popover) => popover.close());
+  }
 
-}}
+  registerPopover(popover: NgbPopover) {
+    this.popovers.push(popover);
+  }
+}
