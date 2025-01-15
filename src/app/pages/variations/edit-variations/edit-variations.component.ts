32c8ab41172fc0dde247a7a0d867b4e880db47e0
Muhammad Shayan Bhatti <shayanbhatti332@gmail.com>
2025-01-15 22:28:50 +0500

    s

--- src/app/pages/variations/edit-variations/edit-variations.component.ts
+++ src/app/pages/variations/edit-variations/edit-variations.component.ts
@@ -62,7 +62,7 @@ console.log(this.variations);
             placeholder: 'Enter name',
             required: true
           },
-          className: 'col-md-3 col-12'
+          className: 'col-md-6 col-12'
         },
         {
           key: 'description',
@@ -72,7 +72,7 @@ console.log(this.variations);
             placeholder: 'Enter a description',
             required: false
           },
-          className: 'col-md-3 col-12'
+          className: 'col-md-6 col-12'
         }
       ]
     }
