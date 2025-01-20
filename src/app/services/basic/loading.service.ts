d7283213e21fd699748d76fd206ba09fabf1ba5a
Muhammad Shayan Bhatti <shayanbhatti332@gmail.com>
2025-01-20 20:27:38 +0500

    ss

--- src/app/services/basic/loading.service.ts
+++ src/app/services/basic/loading.service.ts
@@ -4,15 +4,13 @@ import { NgSimpleStateStoreConfig } from 'ng-simple-state/public-api';
 import { Observable } from 'rxjs';
 
 export interface GlobalLoaderState {
- loader:boolean
-
+  loader: boolean;
 }
 @Injectable({
   providedIn: 'root'
 })
-export class LoadingService extends NgSimpleStateBaseRxjsStore<GlobalLoaderState>{
-
-
+export class LoadingService extends NgSimpleStateBaseRxjsStore<GlobalLoaderState> {
+  loader;
 
   constructor() {
     super();
@@ -22,32 +20,35 @@ export class LoadingService extends NgSimpleStateBaseRxjsStore<GlobalLoaderState
       storeName: 'GlobalLoaderState'
     };
   }
- protected initialState(): GlobalLoaderState {
+  protected initialState(): GlobalLoaderState {
     return {
-    loader:true
-  };
+      loader: true
+    };
   }
-  async showLoader(message = '') {
+  setLoader(flag: boolean) {
+    this.loader = { flag };
+    this.setState((state) => ({ loader: flag }));
+  }
 
+  getLoader(): Observable<any> {
+    return this.selectState((state) => state);
+  }
+
+  async showLoader(message = '') {
     // this.loading = await this.loadingController.create({
     //   cssClass: 'my-loader-class',
     //   spinner: "circles", // Round spinner
     //   translucent: false,
     //   backdropDismiss: false
     // });
-
     // await this.loading.present();
-
-
-
+    this.setLoader(true)
   }
 
   async hideLoader() {
-
     // if (this.loading) {
     //   this.loading.dismiss();
     // }
-
+    this.setLoader(false);
   }
-
 }
