b0cbe4735856d91d948b0c9d794dee815919cbff
Muhammad Shayan Bhatti <shayanbhatti332@gmail.com>
2025-02-27 02:09:13 +0500

    sss

--- src/app/pages/table-booking/view-table-booking/view-table-booking.module.ts
+++ src/app/pages/table-booking/view-table-booking/view-table-booking.module.ts
@@ -4,15 +4,17 @@ import { CommonModule } from '@angular/common';
 import { ViewTableBookingRoutingModule } from './view-table-booking-routing.module';
 import { ViewTableBookingComponent } from './view-table-booking.component';
 import { KtListDetailPageModule } from 'src/app/components/layouts/kt-list-detail-page/kt-list-detail-page.module';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     ViewTableBookingComponent,
-    
+
   ],
   imports: [
     CommonModule,
+    FormsModule,
     ViewTableBookingRoutingModule,
     KtListDetailPageModule
 
