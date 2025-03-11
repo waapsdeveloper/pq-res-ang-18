ffd0690996abc1f981acb221759acd3ca3cbf8ab
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2025-03-11 15:50:02 +0500

    update

--- src/app/pages/orders/list-orders/list-order-item-status/list-order-item-status.module.ts
+++ src/app/pages/orders/list-orders/list-order-item-status/list-order-item-status.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListOrderItemStatusComponent } from './list-order-item-status.component';
 import { FormsModule } from '@angular/forms';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
@@ -11,7 +12,8 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    NgbDropdownModule
   ],
   exports: [ListOrderItemStatusComponent]
 })
