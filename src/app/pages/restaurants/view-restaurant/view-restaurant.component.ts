18e031616eda482d849a1e6b2db2451394d0a36f
Muhammad Shayan Bhatti <shayanbhatti332@gmail.com>
2025-04-16 21:04:44 +0500

    ss

--- src/app/pages/restaurants/view-restaurant/view-restaurant.component.ts
+++ src/app/pages/restaurants/view-restaurant/view-restaurant.component.ts
@@ -10,29 +10,30 @@ import { NetworkService } from 'src/app/services/network.service';
   styleUrl: './view-restaurant.component.scss'
 })
 export class ViewRestaurantComponent {
-
   itemId;
   item;
 
-  constructor(private nav: NavService, private network: NetworkService, public router: Router, public activatedRoute: ActivatedRoute) {
+  constructor(
+    private nav: NavService,
+    private network: NetworkService,
+    public router: Router,
+    public activatedRoute: ActivatedRoute
+  ) {
     this.initialize();
   }
 
   async initialize() {
-
     const rew = await this.activatedRoute.snapshot.params;
-
 
     this.itemId = rew['id'];
 
     const res = await this.network.getRestaurantById(this.itemId);
     console.log(res);
     this.item = res.restaurant;
-
-
-
   }
 
-
-
+  getDayName(index: number): string {
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    return days[index] || 'Day';
+  }
 }
