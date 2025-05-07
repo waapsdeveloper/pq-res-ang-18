2c0e5a330cb3a36cb1d6d9ed0d2375099f409289
ShayanBhatti <shayanbhatti332@gmail.com>
2025-05-07 22:55:40 +0500

    k

--- src/app/pages/branch-config/branch-config.module.ts
+++ src/app/pages/branch-config/branch-config.module.ts
@@ -3,15 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { BranchConfigRoutingModule } from './branch-config-routing.module';
 import { BranchConfigComponent } from './branch-config.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { BtopHeaderModule } from 'src/app/components/btop-header/btop-header.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    BranchConfigComponent
-  ],
-  imports: [
-    CommonModule,
-    BranchConfigRoutingModule
-  ]
+  declarations: [BranchConfigComponent],
+  imports: [CommonModule, BranchConfigRoutingModule, SharedModule, BtopHeaderModule, FormsModule]
 })
-export class BranchConfigModule { }
+export class BranchConfigModule {}
