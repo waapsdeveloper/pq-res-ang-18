7a7f82511479b11c690c4a54926381aa667f09d2
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2025-06-04 01:08:47 +0500

    WSE

--- src/app/pages/roles/roles-routing.module.ts
+++ src/app/pages/roles/roles-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RolesComponent } from './roles.component';
+import { permissionGuard } from '../../guards/permission.guard';
 
 const routes: Routes = [
   {
@@ -15,19 +16,27 @@ const routes: Routes = [
       },
       {
         path: 'list',
-        loadChildren: () => import('./list-roles/list-roles.module').then((m) => m.ListRolesModule)
+        loadChildren: () => import('./list-roles/list-roles.module').then((m) => m.ListRolesModule),
+        data: { entity: 'role', action: 'view' },
+        canActivate: [permissionGuard]
       },
       {
         path: 'add',
-        loadChildren: () => import('./add-roles/add-roles.module').then((m) => m.AddRolesModule)
+        loadChildren: () => import('./add-roles/add-roles.module').then((m) => m.AddRolesModule),
+        data: { entity: 'role', action: 'add' },
+        canActivate: [permissionGuard]
       },
       {
         path: 'view/:id',
-        loadChildren: () => import('./view-roles/view-roles.module').then((m) => m.ViewRolesModule)
+        loadChildren: () => import('./view-roles/view-roles.module').then((m) => m.ViewRolesModule),
+        data: { entity: 'role', action: 'view' },
+        canActivate: [permissionGuard]
       },
       {
         path: 'edit/:id',
-        loadChildren: () => import('./edit-roles/edit-roles.module').then((m) => m.EditRolesModule)
+        loadChildren: () => import('./edit-roles/edit-roles.module').then((m) => m.EditRolesModule),
+        data: { entity: 'role', action: 'edit' },
+        canActivate: [permissionGuard]
       }
     ]
   }
