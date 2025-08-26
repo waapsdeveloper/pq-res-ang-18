e7e7cf1378c4938cadc9232988819b5035f65c8c
ShayanBhatti <shayanbhatti332@gmail.com>
2025-08-26 21:22:22 +0500

    updates

--- src/app/pages/orders/list-orders/list-order-printslip/list-order-printslip.component.ts
+++ src/app/pages/orders/list-orders/list-order-printslip/list-order-printslip.component.ts
@@ -99,7 +99,7 @@ export class ListOrderPrintslipComponent implements OnInit {
     const pdfBlob: Blob = await html2pdf().set(opt).from(section).toPdf().outputPdf('blob');
 
     // Send directly to printer
-    const ok = await this.printingService.printPdf(pdfBlob ,"EPSON M267A");
+    const ok = await this.printingService.printPdf(pdfBlob ,"EPSON TM-T201I Receipt");
     if (ok) {
       console.log('Printed successfully');
     }
